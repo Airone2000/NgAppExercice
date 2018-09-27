7e5a1ee1d7c44999e3b2f6e0be928ae01c3ef71e
Maels1991 <r.wan.guillou+github@gmail.com>
2018-09-27 22:51:02 +0200

    Book detail component

--- src/app/features/book-feature/book-feature.module.ts
+++ src/app/features/book-feature/book-feature.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookHomeComponent } from './book-home/book-home.component';
 import { BookRouterModule } from './book-router/book-router.module';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BookRouterModule
   ],
-  declarations: [BookHomeComponent]
+  declarations: [BookHomeComponent, BookDetailComponent]
 })
 export class BookFeatureModule { }
--- src/app/features/book-feature/book-router/book-router.module.ts
+++ src/app/features/book-feature/book-router/book-router.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 import { BookHomeComponent } from '../book-home/book-home.component'
+import { BookDetailComponent } from '../book-detail/book-detail.component'
 
 
 const ROUTES: Routes = [
-  {path: 'books', component: BookHomeComponent}
+  {path: 'books', component: BookHomeComponent},
+  {path: 'books/:id', component: BookDetailComponent}
 ];
 
 @NgModule({
